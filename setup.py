f4e84491b5bc416e831ee3e204a8699702716fee
Oladapo Adeleke <oladapo_joseph@yahoo.com>
2020-07-20 23:33:38 +0100

    to install the module you need this script

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'Probability_Package',
+     version = '0.1',
+     description= 'Basic Statistics',
+     packages =['Probability_Package'],
+     zip_safe = False)
